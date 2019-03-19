e68c875505771ec9fd2f7a09df6e5f33004f1dda
NGnius <ngniusness@gmail.com>
2019-03-18 21:17:59 -0400

    Fix tests

--- tests/clinfo_test.py
+++ tests/clinfo_test.py
@@ -6,8 +6,8 @@ test_messages = [r'{bot-mention} cl server info "EU-PVE-Official-CardLife-01"', 
 
 class CardLifeTest(testlib.TestCase):
     def test_cardlife_info(self):
-        self.assertIn('cardlife_server_info', self.bot.commands)
-        cl = self.bot.commands['cardlife_server_info']
+        self.assertIn('clinfo', self.bot.commands)
+        cl = self.bot.commands['clinfo']
         for msg_content in test_messages:
             msg_content = msg_content.replace('{bot-mention}', '<@!'+self.bot.user.id+'>')
             msg = testlib.TestMessage(content=msg_content)
